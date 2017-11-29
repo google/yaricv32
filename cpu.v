7411a8625922b562ad9903a0bf1a4f6bb781d36e
Emilian Peev <emilian.peev@gmail.com>
2017-11-29 23:05:58 +0000

    Add support for JAL, JALR

--- cpu.v
+++ cpu.v
@@ -44,6 +44,12 @@ module cpu(
   localparam STYPE_IMM2_END = ITYPE_IMM_END;
   localparam UTYPE_IMM_START = RD_END + 1;
   localparam UTYPE_IMM_END = INSTR_WIDTH - 1;
+  localparam JTYPE_IMM12 = RD_END + 1;
+  localparam JTYPE_IMM19 = JTYPE_IMM12 + 7;
+  localparam JTYPE_IMM11 = JTYPE_IMM19 + 1;
+  localparam JTYPE_IMM1 = JTYPE_IMM11 + 1;
+  localparam JTYPE_IMM10 = JTYPE_IMM1 + 9;
+  localparam JTYPE_IMM20 = JTYPE_IMM10 + 1;
 
   //Load instructions
   localparam LOAD_OPCODE = 7'b0000011;
@@ -62,6 +68,10 @@ module cpu(
   //Load immediate instructions
   localparam LUI_OPCODE = 7'b0110111;
   localparam AUIPC_OPCODE = 7'b0010111;
+
+  //Control tranfer instructions
+  localparam JAL_OPCODE = 7'b1101111;
+  localparam JALR_OPCODE = 7'b1100111;
 
   localparam STAGE_T0 = 0;
   localparam STAGE_T1 = 1;
@@ -87,12 +97,21 @@ module cpu(
   wire [REG_WIDTH-1 : 0] rs2;
   wire [WIDTH-1 : 0] itype_imm;
   wire [WIDTH-1 : 0] stype_imm;
+  wire [WIDTH-1 : 0] utype_imm;
+  wire [WIDTH-1 : 0] jtype_imm;
+
+  wire [WIDTH-1 : 0] load_offset;
+  wire [WIDTH-1 : 0] store_offset;
+  wire [WIDTH-1 : 0] jalr_offset;
 
   assign uart_tx_wire = 1'b1;
 
   initial begin
 `ifdef IVERILOG
-    mem[0] = 32'h00400137;
+  mem[0] = 32'h03c000ef;
+  mem[4] = 32'h038000ef;
+  mem[60] = 32'h00400137;
+  mem[64] = 32'h00008067;
 `endif
   end
 
@@ -107,11 +126,21 @@ module cpu(
   assign rd = ir[RD_END : RD_START];
   assign rs1 = ir[RS1_END : RS1_START];
   assign rs2 = ir[RS2_END : RS2_START];
-  assign itype_imm = regs[rs1] + {ir[ITYPE_IMM_END] ? {20{1'b1}} : 20'b0,
-                      ir[ITYPE_IMM_END : ITYPE_IMM_START]};
-  assign stype_imm = regs[rs1] + {ir[STYPE_IMM2_END] ? {20{1'b1}} : 20'b0,
-                       {ir[STYPE_IMM2_END : STYPE_IMM2_START],
-                         ir[STYPE_IMM1_END : STYPE_IMM1_START]}};
+
+  assign itype_imm = {ir[ITYPE_IMM_END] ? {20{1'b1}} : 20'b0, ir[ITYPE_IMM_END : ITYPE_IMM_START]};
+
+  assign stype_imm = {ir[STYPE_IMM2_END] ? {20{1'b1}} : 20'b0,
+    {ir[STYPE_IMM2_END : STYPE_IMM2_START], ir[STYPE_IMM1_END : STYPE_IMM1_START]}};
+
+  assign utype_imm = {ir[UTYPE_IMM_END : UTYPE_IMM_START], 12'b0};
+
+  assign jtype_imm = {ir[JTYPE_IMM20] ? {12{1'b1}} : 12'b0, {ir[JTYPE_IMM20],
+    ir[JTYPE_IMM19 : JTYPE_IMM12], ir[JTYPE_IMM11], ir[JTYPE_IMM10 : JTYPE_IMM1], 1'b0}};
+
+  assign load_offset = regs[rs1] + itype_imm;
+  assign store_offset = regs[rs1] + stype_imm;
+  assign jalr_offset = regs[rs1] + itype_imm;
+
   integer idx;
   always @(posedge clk) begin
     if (rstn) begin
@@ -126,20 +155,19 @@ module cpu(
             LOAD_OPCODE: begin
               case (ir[FUNCT3_END : FUNCT3_START])
                 LB_FUNCT3: begin
-                  regs[rd] <= {mem[itype_imm][7] ? {24{1'b1}} : 24'b0, mem[itype_imm][7:0]};
+                  regs[rd] <= {mem[load_offset][7] ? {24{1'b1}} : 24'b0, mem[load_offset][7:0]};
                 end
                 LH_FUNCT3: begin
-                  regs[rd] <= {mem[itype_imm][15] ? {16{1'b1}} : 16'b0, mem[itype_imm][15:0]};
+                  regs[rd] <= {mem[load_offset][15] ? {16{1'b1}} : 16'b0, mem[load_offset][15:0]};
                 end
                 LW_FUNCT3: begin
-                  regs[rd] <= mem[itype_imm];
-                  $display("LW rd: %d imm: %d mem: %d\n", rd, itype_imm, mem[itype_imm]);
+                  regs[rd] <= mem[load_offset];
                 end
                 LBU_FUNCT3: begin
-                  regs[rd] <= {24'b0, (mem[itype_imm][7:0])};
+                  regs[rd] <= {24'b0, (mem[load_offset][7:0])};
                 end
                 LHU_FUNCT3: begin
-                  regs[rd] <= {16'b0, (mem[itype_imm][15:0])};
+                  regs[rd] <= {16'b0, (mem[load_offset][15:0])};
                 end
                 default: begin
 `ifdef IVERILOG
@@ -153,13 +181,13 @@ module cpu(
             STORE_OPCODE: begin
               case (ir[FUNCT3_END : FUNCT3_START])
                 SB_FUNCT3: begin
-                  mem[stype_imm][7:0] <= regs[rs2][7:0];
+                  mem[store_offset][7:0] <= regs[rs2][7:0];
                 end
                 SH_FUNCT3: begin
-                  mem[stype_imm][15:0] <= regs[rs2][15:0];
+                  mem[store_offset][15:0] <= regs[rs2][15:0];
                 end
                 SW_FUNCT3: begin
-                  mem[stype_imm] <= regs[rs2];
+                  mem[store_offset] <= regs[rs2];
                 end
                 default: begin
 `ifdef IVERILOG
@@ -171,12 +199,24 @@ module cpu(
             end
 
             LUI_OPCODE: begin
-              regs[rd] <= {ir[UTYPE_IMM_END : UTYPE_IMM_START], 12'b0};
+              regs[rd] <= utype_imm;
               stage_reg <= STAGE_T2;
             end
 
             AUIPC_OPCODE: begin
-              regs[rd] <= pc_reg + {ir[UTYPE_IMM_END : UTYPE_IMM_START], 12'b0} - PC_INC;
+              regs[rd] <= pc_reg + utype_imm - PC_INC;
+              stage_reg <= STAGE_T2;
+            end
+
+            JAL_OPCODE: begin
+              regs[rd] <= pc_reg;
+              pc_reg <= pc_reg + jtype_imm - PC_INC;
+              stage_reg <= STAGE_T2;
+            end
+
+            JALR_OPCODE: begin
+              regs[rd] <= pc_reg;
+              pc_reg <= {jalr_offset[31:1], 1'b0};
               stage_reg <= STAGE_T2;
             end
 
@@ -190,7 +230,7 @@ module cpu(
         end
         STAGE_T2: begin
 `ifdef IVERILOG
-          $display("rd: %d %d\n", rd, regs[rd]);
+          $display("pc_reg: %d\n", pc_reg);
 `endif
           stage_reg <= STAGE_T0;
         end
