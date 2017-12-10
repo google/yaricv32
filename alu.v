14c83cfb95ece5de1dca8208d013518747108fe8
Emilian Peev <emilian.peev@gmail.com>
2017-12-10 14:21:45 +0000

    Add support SLT/SLTU and SLTI/SLTIU instructions

--- alu.v
+++ alu.v
@@ -39,15 +39,19 @@ module alu(
   localparam SHIFT_WIDTH = $clog2(WIDTH);
 
   wire [WIDTH - 1 : 0] carry, b_in, adder;
+  wire slt, sltu;
 
   assign b_in = sub_enable ? ~(b) : b;
   assign eq = a == b;
   assign bgeu = a >= b;
   assign bge = $signed(a) >= $signed(b);
+  assign slt = (!bge) ? 1 : 0;
+  assign sltu = (!bgeu) ? 1 : 0;
 
   assign res = (op == ADD_OP) ? adder : (op == OR_OP) ? (a | b) : (op == XOR_OP) ? (a ^ b) :
                (op == SL_OP) ? (a << shamt) : ((op == SR_OP) && (arith_shift)) ? (a >>> shamt) :
-               (op == SR_OP) ? (a >> shamt) : a & b;
+               (op == SR_OP) ? (a >> shamt) : (op == SLT_OP) ? slt : (op == SLTU_OP) ? sltu :
+               a & b;
 
   genvar i;
   generate
